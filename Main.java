a96603713e14962840e5f027998b7fb1b46f6b3d
bellershaw <braeden.ellershaw@gmail.com>
2021-10-21 21:22:40 -0700

    changed stackpane to a VBox and formatted buttons

--- Main.java
+++ Main.java
@@ -4,27 +4,35 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.geometry.Pos;
 import java.util.*;
 
 public class Main extends Application{
-     Button button; // type button 
 
-    Button nurseButton = new Button("Nurse"); //create nurse button
-    Button doctorButton = new Button("Doctor");  // create doctor button
-    Button patientButton = new Button("Patient") ; // create patient button
+    @Override
+    public void start (Stage primaryStage){
+        primaryStage.setTitle("Login Page");
+        Button expatientButton = new Button("I am an exiting patient");  // create doctor button
+        Button newpatientButton = new Button("I am a new patient") ; // create patient button
+        Button nursedocButton = new Button("I am a doctor or nurse"); //create nurse button
 
-    StackPane layout = new StackPane();
-    layout.getChildren().add(nurseButton);
-    layout.getChildren().add(doctorButton);
-    layout.getChildren().add(patientButton);
+        expatientButton.setPrefSize(200,100);
+        newpatientButton.setPrefSize(200,100);
+        nursedocButton.setPrefSize(200,100);
 
-    Scene scene = new Scene(layout, 600, 600); // scene size
-    primaryStage.setScene(scene);
-    primaryStage.show(); // show it to user
-}
+        VBox layout = new VBox();
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().add(expatientButton);
+        layout.getChildren().add(newpatientButton);
+        layout.getChildren().add(nursedocButton);
+
+        Scene scene = new Scene(layout, 600, 600); // scene size
+        primaryStage.setScene(scene);
+        primaryStage.show(); // show it to user
+    }
 
 
-public static void main(String[] args) {
-    launch(args); // method inside application class that sets up javafx application
+public static void main(String[] args) {launch(args);}; // method inside application class that sets up javafx application
 }
